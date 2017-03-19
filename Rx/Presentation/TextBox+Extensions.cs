8b2cd608d9a4daf92996cc6b35e6fb1baf4f443d
Fatih Şen <fatih.sen.boun@gmail.com>
2017-03-19 18:32:59 +0200

    treeview added

--- Rx/Presentation/TextBox+Extensions.cs
+++ Rx/Presentation/TextBox+Extensions.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 
@@ -17,6 +18,28 @@ namespace ReactiveWPFUserControl.Rx.Presentation {
 			});
 		}
 
-	}
+		public static UIBindingObserver<TSource, TextAlignment> RxTextAlignment<TSource>(this TSource ui) where TSource: TextBox {
+			return new UIBindingObserver<TSource, TextAlignment>(ui, (control, alignment) => {
+				control.TextAlignment = alignment;
+			});
+		}
 
+		public static UIBindingObserver<TSource, CharacterCasing> RxCharacterCasing<TSource>(this TSource ui) where TSource: TextBox {
+			return new UIBindingObserver<TSource, CharacterCasing>(ui, (control, casing) => {
+				control.CharacterCasing = casing;
+			});
+		}
+
+		public static UIBindingObserver<TSource, int> RxMinLines<TSource>(this TSource ui) where TSource: TextBox {
+			return new UIBindingObserver<TSource, int>(ui, (control, minLines) => {
+				control.MinLines = minLines;
+			});
+		}
+
+		public static UIBindingObserver<TSource, int> RxMaxLines<TSource>(this TSource ui) where TSource: TextBox {
+			return new UIBindingObserver<TSource, int>(ui, (control, maxLines) => {
+				control.MaxLines = maxLines;
+			});
+		}
+	}
 }
---
+++ Rx/Presentation/TreeView+Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ReactiveWPFUserControl.Rx.Presentation {
+
+	public static class TreeView_Extensions {
+		
+		public static IObservable<RoutedPropertyChangedEventArgs<object>> RxSelectedItemChanged<TSource>(this TSource ui) where TSource: TreeView {
+			return Observable.FromEventPattern<RoutedPropertyChangedEventHandler<object>, RoutedPropertyChangedEventArgs<object>>(events => {
+				ui.SelectedItemChanged += events;
+			}, events => {
+				ui.SelectedItemChanged -= events;
+			}).Select(e => e.EventArgs);
+		}
+	}
+}
