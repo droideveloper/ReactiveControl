2f98253eeb79840c248e554c8fae75e979b68d73
Fatih Şen <fatih.sen.boun@gmail.com>
2017-03-19 20:51:36 +0200

    added kernel

---
+++ Common/ApplicationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+using System.Threading.Tasks;
+
+namespace ReactiveWPFUserControl.Common {
+	
+	public interface ApplicationType {
+
+		StandardKernel AppComponent { get; }			
+	}
+}
